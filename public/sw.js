ebd61b57e28714a76593a0e1ff7716614a1c008e
hassanrotich111-source <hassanrotich111@gmail.com>
2026-05-15 10:21:10 +0300

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'kira-imports-v1';
+const urlsToCache = [
+  '/kiraimports/',
+  '/kiraimports/index.html',
+  '/kiraimports/assets/index.css',
+  '/kiraimports/assets/index.js',
+  '/kiraimports/images/logo.jpeg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
