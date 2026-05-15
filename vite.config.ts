91accb8fedf36d80ae2fc396a2cc91ab98fccac4
hassanrotich111-source <hassanrotich111@gmail.com>
2026-05-15 20:01:00 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/',
+  base: "/kiraimports/",
   plugins: [react()],
   resolve: {
     alias: {
