9c643a3288771870f256e892c15329e3436b958f
hassanrotich111-source <hassanrotich111@gmail.com>
2026-08-31 20:46:49 +0300

    Use root base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kiraimports/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
